5da4d1f49d8e10d9e10414e3166618b9cad4025a
fredrick amoako <fredrickamoako1@gmail.com>
2023-03-19 11:01:37 +0000

    Add project files.

---
+++ CitiesManager.WebAPI/DataBaseContext/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using CitiesManager.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CitiesManager.WebAPI.DataBaseContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public ApplicationDbContext()
+        {}
+
+        public DbSet<City> Cities { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<City>().HasData(new City
+            {
+                CityID =
+            });
+        }
+
+    }
+}
